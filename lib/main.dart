d1eab656257f836bbee7263e850549b4a47d4733
Aastha Kachchhi <aasthakachchhi@gmail.com>
2025-11-25 23:04:58 +0530

    fews changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
+import 'package:demo1/screen/absentMessage.dart';
+import 'package:demo1/screen/local_cloud_backup.dart';
+import 'package:demo1/screen/recordandreport.dart';
+import 'package:demo1/screen/studentManagement.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screen/absentMessage.dart';
-import 'package:flutter_application_1/screen/local_cloud_backup.dart';
-import 'package:flutter_application_1/screen/recordandreport.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-// --- IMPORTANT: IMPORT YOUR SCREEN FILES ---
-// Make sure the paths are correct based on your project structure.
-// Your home screen (Generator)
-// Your backup screen
-// Your reports screen
-
-void main() {
+void main()  async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -21,15 +22,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Attendance App',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
-      // The home of our app is now the MainScreen widget that holds everything.
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+      ),
+   
       home: const MainScreen(),
     );
   }
 }
 
-// This is our main "wrapper" widget that will manage the bottom navigation.
-// It needs to be a StatefulWidget because the selected tab will change.
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -38,23 +40,19 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  // --- This is our "bookmark" ---
-  // It holds the index (position) of the currently selected tab.
-  // 0 = Generator, 1 = Backup, 2 = Reports
-  int _selectedIndex = 0;
 
-  // --- This is our list of "chapters" (screens) ---
-  // We create a list of all the screen widgets we want to switch between.
+  int _selectedIndex = 0; 
+
+
   static const List<Widget> _screens = <Widget>[
     AbsenceMessageGeneratorScreen(), // Index 0
-    BackupScreen(), // Index 1
-    ReportsApp(), // Index 2
+    BackupScreen(),                  // Index 1
+    ReportScreen(),                    // Index 2
+    StudentManagementScreen(),      // Index 3
   ];
 
-  // This function is called when a tab is tapped.
+  
   void _onItemTapped(int index) {
-    // setState is the magic function that tells Flutter to redraw the screen.
-    // We update our "bookmark" to the new index.
     setState(() {
       _selectedIndex = index;
     });
@@ -69,7 +67,7 @@ class _MainScreenState extends State<MainScreen> {
       // `_screens.elementAt(_selectedIndex)` gets the widget from our list
       // based on which tab is currently selected.
       body: _screens.elementAt(_selectedIndex),
-
+      
       // Here we define our bottom navigation bar.
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -77,14 +75,17 @@ class _MainScreenState extends State<MainScreen> {
             icon: Icon(Icons.chat_bubble),
             label: 'Generator',
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.backup), label: 'Backup'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.backup),
+            label: 'Backup',
+          ),
           BottomNavigationBarItem(
             icon: Icon(Icons.bar_chart),
             label: 'Reports',
           ),
+          
         ],
-        currentIndex:
-            _selectedIndex, // This tells the bar which tab to highlight.
+        currentIndex: _selectedIndex, // This tells the bar which tab to highlight.
         selectedItemColor: const Color(0xFF2563EB), // Color for the active tab.
         onTap: _onItemTapped, // This is what happens when you tap a tab.
       ),
